c4dc6698a67a091c6b7756611f30f5da947281fc
Luana Calisto <luanavenancio060@gmail.com>
2025-01-21 18:05:21 -0300

    creates and implements add item section

---
+++ src/components/add-item-section/addItemSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from '../button/button'
+import TextInput from '../text-input/textInput'
+function AddItemSection() {
+
+  return (
+    <>
+      <TextInput/>
+      <Button title = "Add"/>
+   </>
+  )
+}
+
+export default AddItemSection
